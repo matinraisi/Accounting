f0c9da24d7b1596a15b11fc632ba59ca0900a6f7
matinraisi <raisi.m630@gmail.com>
2024-03-12 00:40:38 +0330

    creatw model Entrance

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -7,3 +8,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User ,on_delete=models.CASCADE)
+    def __str__(self):
+        return  "{}-{}".format(self.text , self.date)
+
+class Entrance(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User ,on_delete=models.CASCADE)
+    def __str__(self):
+        return  "{}-{}".format(self.text , self.date)
